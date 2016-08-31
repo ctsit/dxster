4abb5e4943f05c4ec9cb201d66ac9aaf90afb95a
Amber Allen <amberlallen@ufl.edu>
2016-08-31 16:46:21 -0400

    set up structure for modal pop up button

---
+++ www/functions.js
@@ -0,0 +1,9 @@
+  $(document).ready(function(){
+     $(".button-collapse").sideNav();
+
+     $('select').material_select();
+
+     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  
+  });
