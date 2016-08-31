d001a88c10b8a21567223a12d8ca5a54ae1d4ff8
Christopher Barnes <senrabc@gmail.com>
2016-08-31 15:48:46 -0400

    add bash example and test data set

---
+++ examples/bash/calc_algdx_sample.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+
+# if this is run from the examples folder the context will find the right
+# path to the library
+
+python calc_dxster_sample.py
+#python -c "from .context import dxster; dxster.calc_dxster(0,'normal')"
