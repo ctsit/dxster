4d37c5055cf95fbe028da6f55e12a9617c8275d7
Amber Allen <amberlallen@ufl.edu>
2016-09-01 12:57:16 -0400

    integrate modal pop-ups into calculator. Update some styling.

--- www/functions.js
+++ www/functions.js
@@ -2,8 +2,5 @@
      $('.button-collapse').sideNav();
 
      $('select').material_select();
-
-     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-     $('.modal-trigger').leanModal();
   
   });
